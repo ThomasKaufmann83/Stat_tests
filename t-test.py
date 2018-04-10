0a502f86eda80a189604e62608d87a4b57d5da68
t.kaufmann83@gmx.de <t.kaufmann83@gmx.de>
2018-04-10 11:13:33 +0200

    Initial commit

---
+++ t-test.py
@@ -0,0 +1,45 @@
+import numpy as np
+from scipy.stats import ttest_ind, ttest_ind_from_stats
+from scipy.special import stdtr
+
+np.random.seed(1)
+
+# Create sample data.
+a = np.random.randn(40)
+b = 3*np.random.randn(50000)
+
+# Use scipy.stats.ttest_ind.
+t, p = ttest_ind(a, b, equal_var=False)
+print("ttest_ind:            t = %g  p = %g" % (t, p))
+
+# Compute the descriptive statistics of a and b.
+abar = a.mean()
+avar = a.var(ddof=1)
+na = a.size
+adof = na - 1
+
+bbar = b.mean()
+bvar = b.var(ddof=1)
+nb = b.size
+bdof = nb - 1
+
+# Use scipy.stats.ttest_ind_from_stats.
+t2, p2 = ttest_ind_from_stats(abar, np.sqrt(avar), na,
+                              bbar, np.sqrt(bvar), nb,
+                              equal_var=False)
+print("ttest_ind_from_stats: t = %g  p = %g" % (t2, p2))
+
+# Use the formulas directly.
+tf = (abar - bbar) / np.sqrt(avar/na + bvar/nb)
+dof = (avar/na + bvar/nb)**2 / (avar**2/(na**2*adof) + bvar**2/(nb**2*bdof))
+pf = 2*stdtr(dof, -np.abs(tf))
+
+print("formula:              t = %g  p = %g" % (tf, pf))
+
+print("descriptives:")
+print("mean a = %g" % (abar))
+print("mean b = %g" % (bbar))
+print("st.dev a = %g" % (avar/na))
+print("st.dev b  = %g" % (bvar/nb))
+print("n a = %g" % (na))
+print("n b  = %g" % (nb))
